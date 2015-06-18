d1d1a76882ea98c62e5aabd4b74a6cbc87517cef
albertvalero <albertvalero@gmail.com>
2015-06-18 11:44:20 +0100

    Update cachematrix.R
    Modified vector example code to work with inverse by using the method solve instead of mean

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,40 @@
 ## functions do
 
 ## Write a short comment describing this function
+## Just changed the naming from m to inv
 
 makeCacheMatrix <- function(x = matrix()) {
+
+
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
 ## Write a short comment describing this function
+## Just changed the naming from m to inv and changed the main method from mean to solve
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
+
 }
